10d0416cf2db21971112c28ae129cd75565bb52f
Agus Richard <agus.richard21@gmail.com>
2023-11-09 20:53:56 +0700

    useless function

--- learn-you-some-erlang/code/useless.erl
+++ learn-you-some-erlang/code/useless.erl
@@ -1,5 +1,5 @@
 -module(useless).
--export([add/2, hello/0, greet_and_add_two/1]).
+-export([add/2, hello/0, greet_and_add_two/1, greet_damara/0]).
 
 add(A, B) -> A + B.
 
@@ -8,3 +8,5 @@ hello() -> io:format("Hello World!-n").
 greet_and_add_two(X) ->
     hello(),
     add(X, 2).
+
+greet_damara() -> io:format("I love Damara").
