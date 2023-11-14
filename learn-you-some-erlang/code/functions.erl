7842b3015665fa5e84ce1f520bbb420e7c786e5c
Agus Richard <agus.richard21@gmail.com>
2023-11-14 19:15:17 +0700

    review syntax in functions

--- learn-you-some-erlang/code/functions.erl
+++ learn-you-some-erlang/code/functions.erl
@@ -1,9 +1,12 @@
 -module(functions).
 -compile(export_all).
 
-head([H | _]) -> H.
+greet(male, Name) -> io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) -> io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) -> io:format("Hello, ~s!", [Name]).
 
-second([_, X], _) -> X.
+head([X | _]) -> X.
+second([_, X | _]) -> X.
 
 same(X, X) -> true;
 same(_, _) -> false.
@@ -14,12 +17,39 @@ valid_time({Date = {Y, M, D}, Time = {H, Min, S}}) ->
 valid_time(_) ->
     io:format("Stop feeding me wrong data!~n").
 
-right_age(X) when X > 16, X =< 114 -> true;
+old_enough(X) when X >= 17 -> true;
+old_enough(_) -> false.
+
+right_age(X) when X >= 17, X =< 100 -> true;
 right_age(_) -> false.
 
-greet(male, Name) ->
-    io:format("Hello, Mr. ~s!", [Name]);
-greet(female, Name) ->
-    io:format("Hello, Mrs. ~s!", [Name]);
-greet(_, Name) ->
-    io:format("Hello, ~s!", [Name]).
+animal_talk(Animal) ->
+    Talk =
+        if
+            Animal == cat -> "meow";
+            Animal == beef -> "mooo";
+            Animal == dog -> "bark";
+            true -> "I don't know what to say"
+        end,
+    {Animal, "says" ++ Talk ++ "!"}.
+
+is_she_hot(Name) ->
+    Hotness =
+        if
+            Name == "Damara" -> "She's absolutely hot";
+            Name == "Astiningtiyas" -> "Smoking hot indeed";
+            true -> "Are you kidding me? She's the hottest of them all"
+        end,
+    io:format("OMG, " ++ Hotness ++ "!").
+
+beach(Temperature) ->
+    case Temperature of
+        {celsius, N} when N >= 20, N =< 45 ->
+            'favorable';
+        {kelvin, N} when N >= 293, N =< 318 ->
+            'scientifically favorable';
+        {fahrenheit, N} when N >= 68, N =< 113 ->
+            'favorable in the US';
+        _ ->
+            'avoid beach'
+    end.
